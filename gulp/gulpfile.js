29b8d4e23fe3fbdceff30531728f581454cc712c
Sanesh_Sharudenko <rabota.it.2002@gmail.com>
2024-05-04 04:14:42 +0300

    сделал все кроме иконок в footer

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -16,16 +16,16 @@ const browserSync = require('browser-sync').create();
 
 // перевод sass в css 
 gulp.task('styles', function() {
-  return gulp.src('./new_uber/SASS/*.sass')
+  return gulp.src('../new_uber/dist/sass/*.sass')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./new_uber/css'));
+    .pipe(gulp.dest('../new_uber/dist/css'));
 });
 
 gulp.task('browsersync',function () {
     // инициализация Browsersync
     browserSync.init({ 
       // указываем папку сервера
-      server: { baseDir: './new_uber/' }, 
+      server: { baseDir: '../new_uber/dist/' }, 
       // отключаем уведомления
       notify: false, 
       // отвечает за режим работы: true (подключение к интернет) или false (без подключения интернет)
@@ -34,9 +34,9 @@ gulp.task('browsersync',function () {
   });
 
 gulp.task('watch', function() {
-    gulp.watch('./new_uber/SASS/*.sass', gulp.series('styles'));
-    gulp.watch("./new_uber/CSS/*.css").on('change', browserSync.reload);
-    gulp.watch("./new_uber/*.html").on('change', browserSync.reload);
+    gulp.watch('../new_uber/dist/sass/*.sass', gulp.series('styles'));
+    gulp.watch("../new_uber/dist/css/*.css").on('change', browserSync.reload);
+    gulp.watch("../new_uber/dist/*.html").on('change', browserSync.reload);
 });  
 
 gulp.task('default', gulp.parallel('styles', 'watch', 'browsersync'));
