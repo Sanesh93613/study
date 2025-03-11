26b9647e2f5b74cd52cd75320a896f18017c3f26
Sanesh_Sharudenko <rabota.it.2002@gmail.com>
2025-03-11 21:40:41 +0300

    catalog section without adaptation

--- pulse_tracker/src/js/script.js
+++ pulse_tracker/src/js/script.js
@@ -1,32 +1,76 @@
-// console.log("Биба");
-// $(document).ready(function () {
-//     $(".carousel__slider").slick({
-//         speed: 1200,
-//         adaptiveHeight: false,
-//         prevArrow:
-//             '<button type="button" class="slick-prev"><img src="img/slider/left.svg" alt="left_scroll_button"></button>',
-//         nextArrow:
-//             '<button type="button" class="slick-next"><img src="img/slider/right.svg" alt="right_scroll_button"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                     dots: true,
-//                     arrows: false,
-//                 },
-//             },
-//         ],
-//     });
-// });
+(function ($) {
+    $(function () {
+        $("ul.catalog__tabs").on(
+            "click",
+            "li:not(.catalog__tab_active)",
+            function () {
+                $(this)
+                    .addClass("catalog__tab_active")
+                    .siblings()
+                    .removeClass("catalog__tab_active")
+                    .closest("div.container")
+                    .find("div.catalog__content")
+                    .removeClass("catalog__content_active")
+                    .eq($(this).index())
+                    .addClass("catalog__content_active");
+            }
+        );
+    });
 
-var slider = tns({
+    // $(".catalog-item__link").each(function (i) {
+    //     $(this).on("click", function (e) {
+    //         e.preventDefault();
+    //         $(".catalog-item__content")
+    //             .eq(i)
+    //             .toggleClass("catalog-item__content_active");
+    //         $(".catalog-item__list")
+    //             .eq(i)
+    //             .toggleClass("catalog-item__list_active");
+    //     });
+    // });
+
+    // $(".catalog-item__back").each(function (i) {
+    //     $(this).on("click", function (e) {
+    //         e.preventDefault();
+    //         $(".catalog-item__content")
+    //             .eq(i)
+    //             .toggleClass("catalog-item__content_active");
+    //         $(".catalog-item__list")
+    //             .eq(i)
+    //             .toggleClass("catalog-item__list_active");
+    //     });
+    // });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on("click", function (e) {
+                e.preventDefault();
+                $(".catalog-item__content")
+                    .eq(i)
+                    .toggleClass("catalog-item__content_active");
+                $(".catalog-item__list")
+                    .eq(i)
+                    .toggleClass("catalog-item__list_active");
+            });
+        });
+    }
+
+    toggleSlide(".catalog-item__link");
+    toggleSlide(".catalog-item__back");
+})(jQuery);
+
+const slider = tns({
     container: ".carousel__slider",
     items: 1,
     slideBy: "page",
     autoplay: false,
     nav: false,
-    controlsText: [
-        '<img src="img/slider/left.svg" alt="left_scroll_button"></img>',
-        '<img src="img/slider/right.svg" alt="right_scroll_button">',
-    ],
+    controls: false,
+});
+document.querySelector(".prev").addEventListener("click", function () {
+    slider.goTo("prev");
+});
+
+document.querySelector(".next").addEventListener("click", function () {
+    slider.goTo("next");
 });
