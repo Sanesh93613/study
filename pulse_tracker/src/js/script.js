7ad23b14dc4a8452a2f9b904bc48de245d6ee92c
Sanesh_Sharudenko <rabota.it.2002@gmail.com>
2025-03-16 14:53:08 +0300

    try to do a slider

--- pulse_tracker/src/js/script.js
+++ pulse_tracker/src/js/script.js
@@ -17,7 +17,6 @@
         );
     });
 
-
     function toggleSlide(item) {
         $(item).each(function (i) {
             $(this).on("click", function (e) {
@@ -35,28 +34,3 @@
     toggleSlide(".catalog-item__link");
     toggleSlide(".catalog-item__back");
 })(jQuery);
-
-
-// slider function
-const slider = tns({
-    container: ".carousel__slider",
-    items: 1,
-    slideBy: "page",
-    autoplay: false,
-    nav: true,
-    controls: false,
-    autoHeight: true,
-    responsive: {
-        700: {
-            nav:false,
-        },
-}});
-
-// slider buttons
-document.querySelector(".prev").addEventListener("click", function () {
-    slider.goTo("prev");
-});
-
-document.querySelector(".next").addEventListener("click", function () {
-    slider.goTo("next");
-});
